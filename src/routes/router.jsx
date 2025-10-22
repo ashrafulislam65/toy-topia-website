69bce6b8b08c04644eef6f4e9c6b351a8e6af9b3
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2025-10-23 02:12:44 +0600

    navbar section partially done and coustome color added

---
+++ src/routes/router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+
+const router =createBrowserRouter(
+    [
+        {
+            path:"/",
+            element:<HomeLayout></HomeLayout>,
+        
+        },
+        {
+            path:"/auth",
+            element:<h1>Auth Layout</h1>,
+        },
+        {
+            path:"/*",
+            element:<h1>404 Not Found</h1>,
+        },
+    ]
+
+)
+export default router;
